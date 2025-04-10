f12271c6baf3530ddb158aa92288a161eb89b690
ALMardasDEV <albusaidi9094@gmail.com>
2025-04-10 23:22:21 +0400

    brand componet

---
+++ src/app/[locale]/components/navbar/Brand.tsx
@@ -0,0 +1,23 @@
+
+// import { getTranslations } from "next-intl/server";
+import { Link } from "@/i18n/routing";
+import { Lalezar } from "next/font/google";
+import { useTranslations } from "next-intl";
+// import { getLocale } from "next-intl/server";
+
+const lalezarFont = Lalezar({
+    weight: "400",
+    subsets: ["latin"],
+    display: "swap",
+  });
+
+export default  function Brand(){
+    const t =useTranslations("navbar")
+    // const locale = (await getLocale()).substring(0,2)
+    // const t = await getTranslations("HomePage")
+    return(
+        <div className="mx-6 text-black text-[32px] ">
+            <Link   className={lalezarFont.className} href='/'><p className="flex flex-col relative z-20 text-slate-800">{t("brand-duplicate")} <span className="absolute -z-10 text-blue-600">{t("brand")}</span></p></Link>
+        </div>
+    )
+}
